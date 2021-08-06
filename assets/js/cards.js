2ba6a962dface1f31e115ed9c6ab6669cd4a1c67
Sean Murphy <mail.seanmurphy@gmail.com>
2021-08-06 21:33:20 +0000

    game complete

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -5,9 +5,12 @@ export default class Cards {
 
     constructor() {
         this.moves = 0;
+        this.timeout = false;
+        this.firstCard = null;
         this.container = _.q('#game-container');
         this.movesContainer = _.q('#moves');
         this.cards = [...cards, ...cards];
+        this.count = 0;
     }
 
     shuffle() {
@@ -18,7 +21,7 @@ export default class Cards {
     };
 
     render() {
-        for (let card of this.cards) 
+        for (let card of this.cards)
             this.container.innerHTML += this.new(card);
     };
 
@@ -39,20 +42,56 @@ export default class Cards {
         );
     };
 
-    check(event) {
-        const ref = event.currentTarget.dataset.ref;
-        console.log(ref);
-        this.setMoves();
+    check(event, i) {
+        if (!this.timeout) {
+            const [ref, currentCard] = [event.currentTarget.dataset.ref, event.currentTarget];
+            this.flip(currentCard, 180);
+
+            if (this.firstCard === null) {
+                this.firstCard = { ref: ref, index: i, card: currentCard};
+                return false;
+            }
+            else if (this.firstCard !== null) {
+                this.timeout = true;
+
+                if (ref === this.firstCard.ref) {
+                    _.q('h1').style.color = 'goldenrod';
+                    setTimeout(() => _.q('h1').style.color = '#fff', 1000);
+                    currentCard.style.pointerEvents = 'none';
+                    this.firstCard.card.style.pointerEvents = 'none';
+                    this.firstCard = null;
+                    this.timeout = false;
+                    this.count++;
+
+                    if (this.count === this.cards.length / 2) return true;
+                }
+                else {
+                    setTimeout(() => {
+                        this.flip(currentCard, 0);
+                        this.flip(this.firstCard.card, 0);
+                        this.firstCard = null;
+                        this.timeout = false;
+                    }, 1500);
+                };
+
+                this.incrementMoves();
+            };
+        };
     };
 
-    setMoves() {
+    flip(card, degrees) {
+        const inner = card.querySelector('.card-inner');
+        inner.style.transform = `rotateY(${degrees}deg)`;
+    }
+
+    incrementMoves() {
         _.q('#moves').innerText = `${++this.moves} Moves`;
     };
 
     reset() {
         while (this.container.firstChild)
             this.container.removeChild(this.container.firstChild);
-            
+
         this.movesContainer.innerText = '0 Moves';
     };
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,18 @@ export default class Game {
 
     constructor() {
         this.interval = null;
+        this.stars = _.qAll('.star');
+        this.heading = _.q('h1');
     };
 
     new() {
         this.time = 0;
+        this.heading = 'SNAP!';
         this.cards = new Cards();
         this.cards.reset();
         this.cards.shuffle();
         this.cards.render();
+        this.setStars(3, .5, 'white');
         this.listeners();
         this.timer();
     };
@@ -24,7 +28,9 @@ export default class Game {
 
     listeners() {
         _.q('#reset').onclick = () => this.reset();
-        _.qAll('.card').forEach(card => card.addEventListener('click', event => this.cards.check(event)));
+        _.qAll('.card').forEach((card, i) => card.addEventListener('click', event => {
+            if (this.cards.check(event, i, this.interval)) this.done();
+        }));
     };
 
     timer() {
@@ -32,5 +38,24 @@ export default class Game {
             _.q('#timer').innerText = `${Math.floor(this.time / 60)} Mins ${this.time % 60} Secs`;
             this.time++;
         }, 1000);
+    };
+
+    done() {
+       const rating = this.cards.moves > 18
+        ? 1
+        : this.cards.moves > 12
+            ? 2
+            : 3
+        
+        clearInterval(this.interval);
+        this.heading.innerText = 'You Won!'
+        this.setStars(rating, 1, 'gold');
+    };
+
+    setStars(rating, opacity, color) {
+        for (let i = 0; i < rating; i++) {
+            this.stars[i].style.opacity = opacity;
+            this.stars[i].style.color = color;
+        };
     };
 };
